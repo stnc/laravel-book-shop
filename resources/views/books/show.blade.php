52de8eeefde77045342616c7da77042cf11ed843
stnc <selmantunc@gmail.com>
2018-08-25 16:40:14 +0300

    vue
    vue

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,12 +1,25 @@
+@extends('layouts.app')
 
-<h3>{{ $books->name }}</h3>
 
-<ul>
-       Like olayları ({{ $books->likes->count()}})  //kimler like etmiş .... user bağlantısı
-    @foreach($books->likes as $like)
-        <li>{{ $like->body }}</li>
-    @endforeach
-</ul>
-<task></task>
-<example></example>
-<a href="http://blog.test/author/11">author</a>
+
+
+@section('content')
+
+
+    <h3>{{ $books->name }}</h3>
+
+    <ul>
+        Like olayları ({{ $books->likes->count()}})  //kimler like etmiş .... user bağlantısı
+        @foreach($books->likes as $like)
+            <li>{{ $like->body }}</li>
+        @endforeach
+    </ul>
+
+    <a href="http://blog.test/author/11">author</a>
+
+
+
+    <task></task>
+    <example></example>
+@endsection
+
