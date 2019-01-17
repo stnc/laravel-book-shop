4cc381bc16971cbc0154316b0f00efad132bec85
stnc <selman@yandex.com>
2019-01-17 23:13:42 +0300

    fo0rmamter

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -7,40 +7,37 @@ use Illuminate\Support\Facades\DB;
 use App\Models\A_books;
 use App\Models\User;
 use App\Models\Comments;
+
 class BooksController extends Controller
 {
-
-    public function show($id,Request $request)
-
+    public function show($id, Request $request)
     {
-
-
         //https://startbootstrap.com/template-categories/ecommerce/
 //https://www.kitapyurdu.com/yazar/stefan-zweig/3602.html
       //  DB::enableQueryLog();
-        $books= A_books::find($id);
+        $books = A_books::find($id);
 
         $toplamBegenilme = $books->likes->count();
 
         $tags = collect($books->tags)->implode('name', ',');
-        $userDetails= User::find(1);
+        $userDetails = User::find(1);
 
-        $userDetail=  $userDetails->usersDetail;
-        $comments=  $books->comments;
+        $userDetail = $userDetails->usersDetail;
+        $comments = $books->comments;
 
-        $authorsID=$books->authors->pluck('id');
-        $commentsAuthor=   Comments::whereIn("commentable_id",$authorsID)->where('commentable_type', 'authors')->get();
+        $authorsID = $books->authors->pluck('id');
+        $commentsAuthor = Comments::whereIn("commentable_id", $authorsID)->where('commentable_type', 'authors')->get();
 //        dd($CommentsAuthor);
 //        $query = DB::getQueryLog();
       ////   print_r($query);
-        return view('books.show', compact('books', 'comments',"commentsAuthor", "toplamBegenilme", "tags",'categories',"userDetail"));
+        return view('books.show', compact('books', 'comments', "commentsAuthor", "toplamBegenilme", "tags", 'categories', "userDetail"));
     }
 
 
     public function add()
     {
         $authors = \App\A_authors::create(['name' => 'Free smoke']);
-        $books = \App\A_books::create(['name' => 'Selman tunç','author_id' => $authors->id]);
+        $books = \App\A_books::create(['name' => 'Selman tunç', 'author_id' => $authors->id]);
         $upvote1 = new \App\A_book_author_like;
         $upvote2 = new \App\A_book_author_like;
 
